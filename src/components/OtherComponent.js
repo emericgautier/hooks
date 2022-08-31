65da7e4bdb5c568f5055fdc2de8551fe85c9bc6b
emericgautier <emericgtr@gmail.com>
2022-08-31 15:03:34 +0200

    useContext

---
+++ src/components/OtherComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useContext } from "react";
+import { UserNumber } from "./AppContext";
+
+const OtherComponent = () => {
+  const userNumber = useContext(UserNumber);
+
+  console.log(userNumber);
+
+  return <div>{userNumber}</div>;
+};
+
+export default OtherComponent;
